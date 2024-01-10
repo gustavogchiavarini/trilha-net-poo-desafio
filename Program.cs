b45450288283c0db16cb030a43f0ab8f4d239d69
Gustavo Chiavarini <gustavochiava.dev@gmail.com>
2024-01-10 10:38:54 -0300

    Testes para verificar o funcionamento do sistema.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!!
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "231789", modelo: "Modelo 1", imei:"111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+// Implementado!!
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone  = new Iphone(numero:"45986", modelo: "Modelo2", imei:"2234", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
